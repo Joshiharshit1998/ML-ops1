c00ef0aa204bc6ad26a51b27baea088d9e6dae98
Joshiharshit1998 <harshitjoshi536@gmail.com>
2026-04-01 15:59:56 -0700

    Project Plan created and virtual env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Harshit Joshi",
+    author_email="harshitjoshi536@gmail.com",
+    packages=find_packages()
+)
